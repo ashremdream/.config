5202c39232ef2bdd70f8d4d65cc12cabb20b0c21
Ash Zheng <2017224862@qq.com>
2020-10-04 14:38:21 +0800

    first commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,56 @@
+alias c='clear'
+alias cs='cowsay'
+alias cdiff='colordiff'
+alias gc='git config credential.helper store'
+alias gk='git clone'
+alias ipy='ipython'
+alias kc='kdeconnect-cli'
+alias l='ls -la'
+alias lg='lazygit'
+alias ms='mailsync'
+alias we='weechat'
+alias mt='neomutt'
+alias r='echo $RANGER_LEVEL'
+alias pu='python3 -m pudb'
+alias ra='ranger'
+# ra() {
+	#if [ -z "$RANGER_LEVEL" ]
+	#then
+		#ranger
+	#else
+		#exit
+	#fi
+#}
+alias ne='neofetch'
+alias sc='screenfetch'
+alias g='onefetch'
+alias sra='sudo -E ranger'
+alias sudo='sudo -E'
+alias nvi="nvim"
+alias vi="vim"
+alias reload='source ~/.zshrc'
+alias startup='systemd-analyze plot | display'
+alias gs='git config credential.helper store'
+alias bat='sudo tlp bat'
+alias ac='sudo tlp ac'
+alias gy='git-yolo'
+alias nb='newsboat -r'
+alias nt="sh -c 'cd $(pwd); st' > /dev/null 2>&1 &"
+alias ta='tmux a'
+alias t='tmux'
+alias lo='lsof -p $(fps) +w'
+
+alias re="reboot"
+alias sn="shutdown now"
+
+alias pS='sudo pacman -S'
+alias pSs='pacman -Ss'
+alias pSy='sudo pacman -Sy'
+alias pU='sudo pacman -U'
+alias pSyy='sudo pacman -Syy'
+alias pSyu='sudo pacman -Syu'
+alias pSyyu='sudo pacman -Syyu'
+alias pR="sudo pacman -R"
+alias pRs='sudo pacman -Rs'
+alias pRsn='sudo pacman -Rsn'
+alias pRsnc='sudo pacman -Rsnc'
